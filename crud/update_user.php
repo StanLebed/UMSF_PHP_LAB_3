f970ee70a744ddf22721e8816df2edf6762b199f
StanLebed <61600609+StanLebed@users.noreply.github.com>
2020-07-05 23:47:53 +0300

    Update update_user.php

--- crud/update_user.php
+++ crud/update_user.php
@@ -1,5 +1,7 @@
 <?php 
 require_once "../vendor/connect.php";
+session_start();
+
 $id = $_POST['id'];
 $login = $_POST['login'];
 $password = $_POST['password'];
@@ -7,8 +9,23 @@ $name = $_POST['name'];
 $surname = $_POST['surname'];
 $role = $_POST['role'];
 $lang = $_POST['lang'];
+if ($_SESSION['role']=='admin')
+{
+	if (!empty($login) && !empty($password) && !empty($name) && !empty($surname) && !empty($role))
+	{
+		mysqli_query($connect, "UPDATE `users` SET `login` = '$login', `password` = '$password', `name` = '$name', `surname` = '$surname', `role` = '$role', `language` = '$lang' WHERE `users`.`id` = '$id'");
+		$_SESSION['message'] = "Изменения сохранены!";
+		header("Location: ../profiles/admin.php");
+	}
+	else 
+	{
+		$_SESSION['message'] = "ERROR! Необходимо заполнить все поля!";
+		header("Location: ../profiles/admin.php");
+	}
+}
+else
+{
+	header ("Location: ../errors/ERROR_403.php");
+}
 
-	mysqli_query($connect, "UPDATE `users` SET `login` = '$login', `password` = '$password', `name` = '$name', `surname` = '$surname', `role` = '$role', `language` = '$lang' WHERE `users`.`id` = '$id'");
-	$_SESSION['message'] = "Изменения сохранены!";
-	header('Location: ../index.php');
 ?>
