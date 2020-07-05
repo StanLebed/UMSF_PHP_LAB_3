e819fdbfbde9af9d1a6d85dfc7d1ba234255060b
StanLebed <61600609+StanLebed@users.noreply.github.com>
2020-07-05 18:31:40 +0300

    Update read_userlist.php

--- crud/read_userlist.php
+++ crud/read_userlist.php
@@ -6,36 +6,44 @@ require_once "../vendor/connect.php";
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<!--<title>Таблица пользователей</title>-->
 	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-	<table>
-		<tr>
-			<rh>ID</rh>
-			<rh>Login</rh>
-			<rh>Name</rh>
-			<rh>Surname</rh>
-			<rh>Role</rh>
-			<rh>Lang</rh>
-		</tr>
-		<?php  
-		$users = mysqli_query($connect, "SELECT * FROM  'users' ");
-		$users = mysqli_fetch_all($users, MYSQLI_ASSOC);
-		foreach ($users as $user)
-		{
-			?>
-			<tr>
-				<td><?= user[0] ?></td>
-				<td><?= user[1] ?></td>
-				<td><?= user[3] ?></td>
-				<td><?= user[4] ?></td>
-				<td><?= user[5] ?></td>
-				<td><?= user[6] ?></td>
+	<h2>Таблица пользователей</h2>
+	<div class="userlist">
+		<table>
+			<tr class="userlist_title">
+				<th>ID</th>
+				<th>Login</th>
+				<th>Name</th>
+				<th>Surname</th>
+				<th>Role</th>
+				<th>Language</th>
 			</tr>
-			<?php 
-		}
-		?>
-	</table>
+			<?php  
+			$users = mysqli_query($connect, "SELECT * FROM `users`");
+
+			$users = mysqli_fetch_all($users, MYSQLI_ASSOC);
+
+			if (!mysqli_query($connect, "SELECT * FROM `users`")) 
+			{
+				printf("Error: %s\n", mysqli_error($connect));
+			}
+			foreach ($users as $user)
+			{
+				?>
+				<tr>
+					<td><?= $user['id'] ?></td>
+					<td><?= $user['login'] ?></td>
+					<td><?= $user['name'] ?></td>
+					<td><?= $user['surname'] ?></td>
+					<td><?= $user['role'] ?></td>
+					<td><?= $user['language'] ?></td>
+				</tr>
+				<?php 
+			}
+			?>
+		</table>
+	</div>
 </body>
 </html>
