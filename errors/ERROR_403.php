4ea6bbfd20078f2d9fdaa5e485776061a3aba65a
StanLebed <61600609+StanLebed@users.noreply.github.com>
2020-07-05 22:06:34 +0300

    Update ERROR_403.php

--- errors/ERROR_403.php
+++ errors/ERROR_403.php
@@ -3,12 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>ERROR 403</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 	<div class="error_403">
-		<a href="index.php"><button class="button go_back">Назад</button></a><br>
-		<img class="error_403_img" src="img/error_403_forbidden.jpg" alt="error_403">
+		<a href="../index.php"><button class="button go_back">Назад</button></a><br>
 	</div>
 </body>
 </html>
